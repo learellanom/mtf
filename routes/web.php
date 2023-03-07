b48ac22c943c3d857b46c437d86a11990309d838
antoniopuerta <antoniolenovo115@gmail.com>
2023-03-07 09:09:50 -0400

    Plugin PWA

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+Route::get('/offline', function(){
+    return view('vendor.laravelpwa.offline');
+});
 
 Route::get('/', function () {
     return view('auth.login');
