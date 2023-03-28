ae01b2a21a5d34e8a613c560347fd4f6e0339c20
Antonio Puerta <67332538+KumbaC@users.noreply.github.com>
2023-03-28 14:42:06 -0400

    MTFBASE 27-03-2023 14:42

--- resources/views/transactions/edit.blade.php
+++ resources/views/transactions/edit.blade.php
@@ -5,7 +5,7 @@
 @section('title', 'Movimientos')
 @section('content_header')
 
-    <h1 class="text-center text-dark font-weight-bold">NUEVO MOVIMIENTO <i class="fas fa-exchange-alt"></i> </h1></a>
+    <h1 class="text-center text-dark font-weight-bold">MODIFICAR MOVIMIENTO <i class="fas fa-exchange-alt"></i> </h1></a>
 
 
 @stop
