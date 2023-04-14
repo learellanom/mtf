c0f7fbfe66691920f47211a94da26461c844fe7b
Antonio Puerta <67332538+KumbaC@users.noreply.github.com>
2023-04-14 07:57:38 -0400

    MTFBASE 14-04-2023 8:00

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,10 +35,6 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
 
-                Route::middleware('web')
-                ->prefix('admin')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/admin.php'));
         });
     }
 
