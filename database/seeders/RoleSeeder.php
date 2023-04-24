839c813376108901e7f762806a7d74b0bde095d3
Antonio Puerta <67332538+KumbaC@users.noreply.github.com>
2023-04-24 12:03:13 -0400

    MTFBASE 24-04-2023 12:03

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -16,9 +16,11 @@ class RoleSeeder extends Seeder
     {
         $role1 = Role::create(['name'=>'Administrador']);
         $role2 = Role::create(['name'=>'Agente']);
+        $role3 = Role::create(['name'=>'Supervisor']);
 
-        Permission::create(['name' => 'home','description' => 'Ver el inicio del sistema'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'dashboardtest','description' => 'Ver graficos del sistema'])->syncRoles([$role1, $role2]);
+
+        Permission::create(['name' => 'home','description' => 'Ver el inicio del sistema'])->syncRoles([$role1, $role2, $role3]);
+        Permission::create(['name' => 'dashboardtest','description' => 'Ver graficos del sistema'])->syncRoles([$role1, $role2, $role3]);
 
 
         Permission::create(['name' => 'roles.index','description' => 'Ver roles del sistema'])->assignRole($role1);
@@ -31,10 +33,10 @@ class RoleSeeder extends Seeder
         Permission::create(['name' => 'clients.update','description' => 'Modificar clientes del sistema'])->assignRole($role1);
         Permission::create(['name' => 'clients.create','description' => 'Crear clientes al sistema'])->assignRole($role1);
 
-        Permission::create(['name' => 'groups.index','description' => 'Ver grupos del sistema'])->assignRole($role1);
-        Permission::create(['name' => 'groups.edit','description' => 'Editar grupos del sistema'])->assignRole($role1);
-        Permission::create(['name' => 'groups.update','description' => 'Modificar grupos del sistema'])->assignRole($role1);
-        Permission::create(['name' => 'groups.create','description' => 'Crear grupos al sistema'])->assignRole($role1);
+        Permission::create(['name' => 'groups.index','description' => 'Ver grupos del sistema'])->syncRoles([$role1, $role3]);
+        Permission::create(['name' => 'groups.edit','description' => 'Editar grupos del sistema'])->syncRoles([$role1, $role3]);
+        Permission::create(['name' => 'groups.update','description' => 'Modificar grupos del sistema'])->syncRoles([$role1, $role3]);
+        Permission::create(['name' => 'groups.create','description' => 'Crear grupos al sistema'])->syncRoles([$role1, $role3]);
 
         Permission::create(['name' => 'users.index','description' => 'Ver usuarios del sistema'])->assignRole($role1);
         Permission::create(['name' => 'users.password','description' => 'Cambiar contraseña de los usuarios'])->assignRole($role1);
@@ -54,27 +56,27 @@ class RoleSeeder extends Seeder
         Permission::create(['name' => 'wallets.edit','description' => 'Modificar las cajas del sistema'])->assignRole($role1);
         Permission::create(['name' => 'wallets.create','description' => 'Crear cajas al sistema'])->assignRole($role1);
 
-        Permission::create(['name' => 'estadisticasDetalle.index','description' => 'Detalles de movimientos'])->syncRoles([$role1, $role2]);
+        Permission::create(['name' => 'estadisticasDetalle.index','description' => 'Detalles de movimientos'])->syncRoles([$role1, $role2, $role3]);
         Permission::create(['name' => 'estadisticasDetalle.statisticsResumenCliente','description' => 'Resumen de movimientos por cliente'])->assignRole($role1);
         Permission::create(['name' => 'estadisticasDetalle.statisticsResumenUsuario','description' => 'Resumen de movimientos por agente'])->assignRole($role1);
         Permission::create(['name' => 'estadisticasDetalle.statisticsResumenWallet','description' => 'Resumen de movimientos por cajas'])->assignRole($role1);
 
-        Permission::create(['name' => 'estadisticasDetalle.statisticsResumenConciliacionFecha','description' => 'Conciliación por fecha'])->assignRole($role1);
-        Permission::create(['name' => 'estadisticasDetalle.statisticsResumenConciliacionFechaGrupo','description' => 'Conciliación por grupos'])->assignRole($role1);
+        Permission::create(['name' => 'estadisticasDetalle.statisticsResumenConciliacionFecha','description' => 'Conciliación por fecha'])->syncRoles([$role1, $role3]);
+        Permission::create(['name' => 'estadisticasDetalle.statisticsResumenConciliacionFechaGrupo','description' => 'Conciliación por grupos'])->syncRoles([$role1, $role3]);
 
 
-        Permission::create(['name' => 'transactions_master.index', 'description' => 'Ver transacciónes master'])->assignRole($role1);
-        Permission::create(['name' => 'transactions_master.edit', 'description' => 'Modificar transacciónes master'])->assignRole($role1);
-        Permission::create(['name' => 'transactions_master.create', 'description' => 'Crear transacciónes master'])->assignRole($role1);
+        Permission::create(['name' => 'transactions_master.index', 'description' => 'Ver transacciónes master'])->syncRoles([$role1, $role3]);
+        Permission::create(['name' => 'transactions_master.edit', 'description' => 'Modificar transacciónes master'])->syncRoles([$role1, $role3]);
+        Permission::create(['name' => 'transactions_master.create', 'description' => 'Crear transacciónes master'])->syncRoles([$role1, $role3]);
 
 
 
-        Permission::create(['name' => 'transactions.index','description' => 'Ver transacciónes'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'transactions.create', 'description' => 'Crear transacciónes'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'transactions.create_efectivo', 'description' => 'Crear transacciónes (CASH)'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'transactions.edit_efectivo', 'description' => 'Modificar transacciónes (CASH)'])->syncRoles([$role1, $role2]);
+        Permission::create(['name' => 'transactions.index','description' => 'Ver transacciónes'])->syncRoles([$role1, $role2, $role3]);
+        Permission::create(['name' => 'transactions.create', 'description' => 'Crear transacciónes'])->syncRoles([$role1, $role2, $role3]);
+        Permission::create(['name' => 'transactions.create_efectivo', 'description' => 'Crear transacciónes (CASH)'])->syncRoles([$role1, $role2, $role3]);
+        Permission::create(['name' => 'transactions.edit_efectivo', 'description' => 'Modificar transacciónes (CASH)'])->syncRoles([$role1, $role2, $role3]);
 
-        Permission::create(['name' => 'transactions.edit', 'description' => 'Modificar transacciónes'])->syncRoles([$role1, $role2]);
+        Permission::create(['name' => 'transactions.edit', 'description' => 'Modificar transacciónes'])->syncRoles([$role1, $role2, $role3]);
 
         Permission::create(['name' => 'transactions.credit', 'description' => 'Crear credito a la caja'])->assignRole($role1);
         Permission::create(['name' => 'transactions.credit_edit', 'description' => 'Modificar credito a la caja'])->assignRole($role1);
