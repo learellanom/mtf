137fad7997f440faf42008b933312799d9da8a2d
Antonio Puerta <67332538+KumbaC@users.noreply.github.com>
2023-05-29 11:15:58 -0400

    MTFBASE 05-29-23 11:15

--- database/migrations/2023_03_09_161229_create_transactions_table.php
+++ database/migrations/2023_03_09_161229_create_transactions_table.php
@@ -33,6 +33,7 @@ return new class extends Migration
             $table->datetime('transaction_date'); //FECHA DE LA TRANSACCIÓN
             $table->double('percentage_base')->nullable(); //PORCENTAJE DE LA GANANCIA
             $table->double('amount_commission_base')->nullable(); //GANANCIA DE LA COMOSION
+            $table->double('amount_total_base')->nullable(); //GANANCIA DE LA COMOSION
             $table->timestamps(); //CREACION Y MODIFICACION POR DEFECTO (TOMADO POR EL SERVIDOR)
         });
     }
