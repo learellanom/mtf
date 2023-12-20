9851fc1c3e6b0a4683d1c7129581f3bcfa7412ac
admin <tioammimtf@gmail.com>
2023-12-20 17:54:11 -0400

    mtfbase 20-12-2023 1753

--- app/Models/Commission_usdt.php
+++ app/Models/Commission_usdt.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Commission_usdt extends Model
 {
     use HasFactory;
+
+
+       //Relación uno a muchos
+    public function type_transaction(){
+        return $this->belongsTo(Type_transaction::class);
+    }
+        //Relación uno a muchos
+    public function user(){
+        return $this->belongsTo(user::class);
+    }
+    //Relación uno a muchos
+    public function group(){
+        return $this->belongsTo(group::class, 'group_id');
+    }
+
+    public function wallet() {
+        return $this->belongsTo(group::class, 'wallet_id');
+    }
+
+
+
 }
