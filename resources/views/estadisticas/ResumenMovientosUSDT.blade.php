7be14fd587316915cb56def6f213cbbd691b54a0
admin <tioammimtf@gmail.com>
2024-06-12 17:31:12 -0400

    mtfbase 12-06-2024 1731

--- resources/views/estadisticas/ResumenMovientosUSDT.blade.php
+++ resources/views/estadisticas/ResumenMovientosUSDT.blade.php
@@ -421,7 +421,7 @@
             
             <div class="card mb-4">
                 <div class="card-header" style="background-color: red; color: white">
-                    <h3 class="card-title text-uppercase font-weight-bold">Grupos sin asignar en filtro</h3>
+                    <h3 class="card-title text-uppercase font-weight-bold">Grupos con pagos USDT sin asignar en filtro</h3>
                 </div>
                 <div class="card-body">
 
@@ -430,7 +430,7 @@
                     <div class="row justify-content-center text-center align-items-center">
                         <div class="col-12 col-md-6 col-lg-6 col-xl-4 justify-content-center text-center align-items-center">
                             <div class="mt-4 mb-4">
-                                <label>Grupos sin asignar</label>
+                                <label>Grupos con Pagos USDT sin asignar</label>
                             </div>                            
                             <select multiple="multiple" id="my-select6" name="" style="width:50%; height: 250px;"></select>
                         </div>  
