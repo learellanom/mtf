158a796897f59f83ab752527517eaee095b61732
admin <tioammimtf@gmail.com>
2024-06-20 17:30:42 -0400

    mtfbase 20-06-2024 1730

--- resources/views/materials/recepcion_edit.blade.php
+++ resources/views/materials/recepcion_edit.blade.php
@@ -263,7 +263,7 @@
             let myDays;
             myDays = 4;
             myDays = 30;
-            // myDays = 240;
+             myDays = 240;
 
             let myDateBefore = new Date();
                 myDateBefore.setDate(myDateBefore.getDate() - myDays);
