377f7eb4e6172cc03da1c89dd94b6d176847863d
admin <tioammimtf@gmail.com>
2024-09-23 17:18:25 -0400

    mtfbase 23-09-2024 1718

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->enum('type', [1, 2])->nullable()->default(1); // 1 : Administativo, 2: externo;
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
