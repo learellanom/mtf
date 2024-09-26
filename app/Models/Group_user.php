0d7bb9eacd31ad70d66f1891b9162edf553c10b8
admin <tioammimtf@gmail.com>
2024-09-26 16:55:18 -0400

    mtfbase 26-09-2024 1655

---
+++ app/Models/Group_user.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
+use Illuminate\Notifications\Notifiable;
+
+class Group_user extends Model
+{
+    use Notifiable, HasFactory;
+}
