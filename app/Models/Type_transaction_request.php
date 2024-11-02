13624c97b9f8d8b0db2df0a4f9f2fd4d509c0bd4
admin <tioammimtf@gmail.com>
2024-11-02 12:05:20 -0400

    mtfbase 02-11-2024 1205

---
+++ app/Models/Type_transaction_request.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Type_transaction_request extends Model
+{
+    use \OwenIt\Auditing\Auditable;
+    use HasFactory, HasFactory, Notifiable;
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+
+}
