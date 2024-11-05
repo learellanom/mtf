b8c0586445d5053debbeb9d7c192f84e9cd60508
admin <tioammimtf@gmail.com>
2024-11-04 23:34:22 -0400

    mtfbase 11/04/2024 2334@

---
+++ resources/views/type_transaction_requests/create.blade.php
@@ -0,0 +1,49 @@
+@extends('adminlte::page')
+
+@section('title', 'MTF| Tipo de Material')
+
+@section('content_header')
+
+    <h1 class="text-center text-dark font-weight-bold">CREAR TIPO MATERIAL <i class="fab fa-bitcoin"></i> </h1></a>
+
+
+@stop
+
+@section('content')
+<div class="d-flex justify-content-center">
+    <div class="card col-md-4">
+        <div class="card-body">
+            {!! Form::open(['route' => 'type_materials.store', 'autocomplete' => 'off', 'files' => true]) !!}
+
+
+            <div class="form-group">
+                {!! Form::Label('name', "Tipo de material:") !!}
+                {!! Form::text('name', null, ['class' => 'form-control', 'required' => true]) !!}
+
+
+                @error('name')
+
+                <span class="text-danger">{{$message}}</span>
+
+                @enderror
+
+            </div>
+
+
+            <div class="form-group">
+                {!! Form::Label('description', "Observación:") !!}
+                {!! Form::textarea('description', null, ['class' => 'form-control', 'required' => true]) !!}
+
+                @error('description')
+                    <small class="text-danger">{{$message}}</small>
+                @enderror
+            </div>
+
+            {!! Form::Submit('GUARDAR', ['class' => 'btn btn-primary btn-block font-weight-bold']) !!}
+
+            {!! Form::close() !!}
+        </div>
+    </div>
+
+</div>
+@endsection
---
+++ resources/views/type_transaction_requests/index.blade.php
@@ -0,0 +1,106 @@
+@extends('adminlte::page')
+
+@section('title', 'Tipo de Solicitud')
+
+@section('content_header')
+
+    <h1 class="text-center text-dark font-weight-bold">{{ __('TIPO DE SOLICITUD') }} <i class="fab fa-bitcoin"></i> </h1></a>
+
+
+@stop
+
+@section('content')
+
+
+<a class="btn btn-dark" title="Crear Tipo de Moneda" href={{ route('type_transaction_requests.create') }}>
+    <i class="fas fa-plus-circle"></i>
+    <span class="d-none d-lg-inline-block text-uppercase font-weight-bold">{{ __('Crear') }}</span>
+    <span class="d-none d-md-inline-block text-uppercase font-weight-bold">{{ __('Tipo de Solicitud') }}</span>
+</a>
+<br><br>
+{{-- Compressed with style options / fill data using the plugin config --}}
+
+<div class="row">
+
+    <div class="col-md-12">
+        <div class="card mb-4">
+            <div class="card-header">
+                <h3 class="card-title font-weight-bold">{{ __('TIPO DE SOLICITUD') }} <i class="fab fa-bitcoin"></i></h3>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-12">
+                <table class="table table-bordered table-responsive-lg" id="moneda">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Descripción</th>
+                            <th class="text-center">Editar</th>
+                            <th class="text-center">Eliminar</th>
+                        </tr>
+                    </thead>
+
+
+
+                    @foreach($Type_material as $material)
+                        <tr>
+
+                            <td>{!! $material->name !!}</td>
+                            <td>{!! $material->description !!}</td>
+
+                            <td class="text-center">
+
+                            <a class="btn btn-xl text-primary mx-1 shadow" href="{{ route('type_materials.edit', $material) }}" title="Editar">
+                                <i class="fa fa-lg fa-fw fa-pen"></i>
+                            </a>
+
+                            </td>
+                            <td class="text-center">
+                                <form method="post" action="{{ route('type_materials.destroy', $material->id) }}">
+                                    @csrf
+                                    @method('delete')
+                                <button class="btn btn-xl text-danger mx-1 shadow" type="submit" title="Borrar">
+                                    <i class="fa fa-lg fa-fw fa-trash"></i>
+                                </button>
+                                </form>
+                            </td>
+
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+@section('js')
+<script type="text/javascript">
+$(document).ready( function () {
+    $('#moneda').DataTable({
+
+        language: {
+            "decimal": "",
+            "emptyTable": "No hay información",
+            "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+            "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+            "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ Entradas",
+            "loadingRecords": "Cargando...",
+            "processing": "Procesando...",
+            "search": "Buscar:",
+            "zeroRecords": "Sin resultados encontrados",
+            "paginate": {
+                "first": "Primero",
+                "last": "Ultimo",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },
+    });
+
+} );
+</script>
+@endsection
