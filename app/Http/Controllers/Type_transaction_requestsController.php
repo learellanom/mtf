6bf0d67c37536383cbda5748cc3d7ba7cdf0beae
admin <tioammimtf@gmail.com>
2024-11-07 19:52:17 -0400

    mntfbase 07-11-2024 1752

--- app/Http/Controllers/Type_transaction_requestsController.php
+++ app/Http/Controllers/Type_transaction_requestsController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Redirect;
-use App\Models\Type_transaction_requests;
+use App\Models\Type_transaction_request;
 
-class Type_transaction_requestController extends Controller
+
+class Type_transaction_requestsController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -14,7 +16,7 @@ class Type_transaction_requestController extends Controller
     public function index()
     {
         //
-        $Type_transaction_requests = Type_transaction_requests::all();
+        $Type_transaction_requests = Type_transaction_request::all();
         return view('type_transaction_requests.index', compact('Type_transaction_requests'));        
     }
 
@@ -33,7 +35,7 @@ class Type_transaction_requestController extends Controller
     public function store(Request $request)
     {
         //
-        Type_transaction_requests::create($request->all());
+        Type_transaction_request::create($request->all());
 
         flash()->addSuccess('Nuevo Tipo de Solicitud creada con exito.', 'Tipo de Solicitud', ['timeOut' => 3000]);
 
@@ -54,7 +56,7 @@ class Type_transaction_requestController extends Controller
     public function edit(string $id)
     {
         //
-        $Type_transaction_requests = Type_transaction_requests::find($id);
+        $Type_transaction_requests = Type_transaction_request::find($id);
         
 
         return view('type_transaction_requests.edit', compact('Type_transaction_requests'));        
@@ -66,7 +68,7 @@ class Type_transaction_requestController extends Controller
     public function update(Request $request, string $id)
     {
         //
-        $Type_transaction_requests = Type_material::find($$id);
+        $Type_transaction_requests = Type_transaction_request::find($$id);
         $Type_transaction_requests->update($request->all());
         flash()->addInfo('Tipo de Solicitud modificada..', 'Tipo de solicitud', ['timeOut' => 3000]);
         return Redirect::route('type_transaction_requests.index');        
@@ -78,12 +80,12 @@ class Type_transaction_requestController extends Controller
     public function destroy(string $id)
     {
         //
-        $Type_transaction_requests = Type_material::find($id);
+        $Type_transaction_requests = Type_transaction_request::find($id);
         
         $Type_transaction_requests->delete();
 
         flash()->addError('Tipo de Solicitud', 'Tipo de Solicitud Eliminada: ' . $Type_transaction_requests->name,  ['timeOut' => 2000]);
         return Redirect::route('type_transaction_requests.index');    
-            
+
     }
 }
