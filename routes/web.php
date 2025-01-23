d1dc8739dba754b729fff12042083e61e1bbacb1
admin <tioammimtf@gmail.com>
2025-01-23 19:50:36 -0400

    mtfbase 23-01-2025

--- routes/web.php
+++ routes/web.php
@@ -326,6 +326,11 @@ Route::group(['middleware' => 'auth'], function () {
     ->middleware('can:estadisticaCajaMayor')
     ->name('estadisticasCajaMayor');
 
+    Route::get('cajaMayorCuadroMovimientos',
+    [statisticsController::class, 'cajaMayorCuadroMovimientos'])
+    ->middleware('can:estadisticaCajaMayor')
+    ->name('cajaMayorCuadroMovimientos');
+
     // Route::resource('cajamayor', EnterpriseController::class)->middleware('auth')->except('show')->middleware('can:enterprise.index')->names('enterprise');
     Route::resource('cajamayor', EnterpriseController::class)->middleware('auth')->names('enterprise');
 });
