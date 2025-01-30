27c0cb4c6852ca65ea34de3f6f73121236b77444
admin <tioammimtf@gmail.com>
2025-01-30 19:52:53 -0400

    mtfbase 30-01-2025

--- app/Http/Controllers/statisticsController.php
+++ app/Http/Controllers/statisticsController.php
@@ -7098,11 +7098,11 @@ class statisticsController extends Controller
                 mtf.transactions.type_transaction_id,
                 type_transactions.name
             order by
-                mtf.transactions.wallet_id ASC,
-                mtf.transactions.group_id ASC
+                mtf.transactions.group_id ASC,            
+                mtf.transactions.wallet_id ASC
         ";
 
-        // dd($myQuery);
+         // dd($myQuery);
         
         $Recargas = DB::select($myQuery);
         // dd($Recargas);
@@ -10029,7 +10029,7 @@ public function cajaMayorCuadroMovimientos(request $request)
     $grupo                          = app(GroupController::class)->getGroups2();
     $typeTransactions               = $this->getTypeTransactions();
     $cajaMayor                      = $this->getEnterprise($request);
-    
+    dd($wallet2);
     $balance                        = 0;
     $balanceBefore                  = 0;
     
@@ -10054,26 +10054,26 @@ public function cajaMayorCuadroMovimientos(request $request)
         // dd($myJsonData['groupsEntrada1']);
         // dd($myJsonData);
         $RecargasWallet             = $this->ResumenEntradaWallet($cajaMayorWallets, $myFechaDesde, $myFechaHasta);
-        
+        // dd($RecargasWallet);
         // leam
         $myGroups                   = $myJsonData['groupsEntrada1'];
         $transaccionesGrupoComision = $this->ResumenEntradaWalletComision($cajaMayorWallets, null, $myFechaDesde, $myFechaHasta);
         
         $myGroups                   = $myJsonData['groupsSalida1'];
         // $transaccionesGrupoSalida   = $this->ResumenSalidaWalletGroup($myWallet, $myGroups);
-        $transaccionesGrupoSalida   = $this->ResumenSalidaWalletGroup($myWallet);
+        $transaccionesGrupoSalida   = $this->ResumenSalidaWalletGroup($cajaMayorWallets);
         
         $myGroups                   = $myJsonData['groupsSalida2'];
         // $transaccionesGrupoSalida   = $this->ResumenSalidaWalletGroup($myWallet, $myGroups);
-        $transaccionesGrupoSalida   = $this->ResumenSalidaWalletGroup($myWallet);        
+        $transaccionesGrupoSalida   = $this->ResumenSalidaWalletGroup($cajaMayorWallets);        
 
         $myGroups                   = $myJsonData['groupsSalida3'];
         // $transaccionesGrupoSalida   = $this->ResumenSalidaWalletGroup($myWallet, $myGroups);
-        $transaccionesGrupoSalida   = $this->ResumenSalidaWalletGroup($myWallet);
+        $transaccionesGrupoSalida   = $this->ResumenSalidaWalletGroup($cajaMayorWallets);
 
         $request->groups = $myJsonData['walletsSalida3'];
         // $transaccionesGrupoSalida   = $this->ResumenSalidaWalletGroup($myWallet, $myGroups);
-        $transaccionesGrupoSalida   = $this->ResumenSalidaWalletGroup($myWallet);
+        $transaccionesGrupoSalida   = $this->ResumenSalidaWalletGroup($cajaMayorWallets);
 
 
     }else{
@@ -10151,38 +10151,32 @@ public function cajaMayorCuadroMovimientos(request $request)
     *
     *
     * Resumen SalidaWalletGroup
-    * Pagos que ha realizado un wallet
+    * Pagos que ha realizado un wallet de la caja mayor
     * comisiones 
     */
-    function ResumenSalidaWalletGroup($wallet = 0 , $groups =""){
+    function ResumenSalidaWalletGroup($cajaMayorWallets = "", $groups ="", $fechaDesde = "", $fechaHasta=""){
 
+        if ($cajaMayorWallets == "") return $recargas3;
 
+        $myWallets = [];
+        if ($cajaMayorWallets != ""){
+            $myWallets = implode(",",$cajaMayorWallets);
 
-        $myWalletDesde = $wallet;
-        $myWalletHasta = $wallet;
-               
+        }
+
 		$myGroups = [];
-        if ($groups){
+        if ($groups != ""){
             $myGroups = implode(",",$request->groups);
         }
 
         $myFechaDesde = "2001-01-01";
         $myFechaHasta = "9999-12-31";
-        if ($request->fechaDesde){
-            $myFechaDesde = $request->fechaDesde;
+        if ($fechaDesde != ""){
+            $myFechaDesde = $fechaDesde;
         }
-        if ($request->fechaHasta){
-            $myFechaHasta = $request->fechaHasta;
+        if ($fechaHasta != ""){
+            $myFechaHasta = $fechaHasta;
         }
-
-        //$myFechaDesde = "2001-01-01";
-        //$myFechaHasta = "9999-12-31";
-
-        $horaDesde      = " 00:00:00";
-        $horaHasta      = " 23:59:00";
-
-        $myFechaDesde   = $myFechaDesde . $horaDesde;
-        $myFechaHasta   = $myFechaHasta . $horaHasta;
 
         $myTable        = "mtf.transactions";
 
@@ -10219,10 +10213,10 @@ public function cajaMayorCuadroMovimientos(request $request)
             left join   mtf.groups              on mtf.Transactions.group_id            = mtf.groups.id
             where
                     status = 'Activo'
-                and wallet_id               between $myWalletDesde              and     $myWalletHasta                    
-                and group_id                in($myGroups)
+                and wallet_id               in($myWallets)
+                $myFiltroGrupos
                 and type_transaction_id     in (11)
-                and transaction_date        between '$myFechaDesde'             and     '$myFechaHasta'
+                and transaction_date        between '$myFechaDesde 00:00:00'             and     '$myFechaHasta 23:59:00'
             group by
                 mtf.transactions.wallet_id,
                 wallets.name,
