b22ffa860d56c264364bfd2d03043673ea878016
admin <tioammimtf@gmail.com>
2025-11-26 11:34:30 -0400

    test2

---
+++ arregla2.bat
@@ -0,0 +1,19 @@
+git filter-branch -f --tree-filter "rm -f 'backups/mtf20241102- 73046.sql'" HEAD
+git filter-branch -f --tree-filter "rm -f 'backups/mtf20241221- 73045.sql'" HEAD
+git filter-branch -f --tree-filter "rm -f 'backups/mtf20240923- 73045.sql'" HEAD
+git filter-branch -f --tree-filter "rm -f 'backups/mtf20240911- 73045.sql'" HEAD
+git filter-branch -f --tree-filter "rm -f 'backups/mtf20241024- 73045.sql'" HEAD
+git filter-branch -f --tree-filter "rm -f 'backups/mtf20241015- 73045.sql'" HEAD
+git filter-branch -f --tree-filter "rm -f 'backups/mtf20241001- 73045.sql'" HEAD
+git filter-branch -f --tree-filter "rm -f 'backups/mtf20250118 - 51117.sql'" HEAD
+git filter-branch -f --tree-filter "rm -f 'backups/mtf20241212 - 51117.sql'" HEAD
+git filter-branch -f --tree-filter "rm -f 'backups/mtf20240925- 73045.sql'" HEAD
+git filter-branch -f --tree-filter "rm -f 'backups/mtf20241031- 84133.sql'" HEAD
+git filter-branch -f --tree-filter "rm -f 'backups/mtf20241102- 93652.sql'" HEAD
+git filter-branch -f --tree-filter "rm -f 'backups/mtf20241116- 73045.sql'" HEAD
+git filter-branch -f --tree-filter "rm -f 'backups/mtf20241207 - 51117.sql'" HEAD
+git filter-branch -f --tree-filter "rm -f 'backups/mtf20241221- 73045.sql'" HEAD
+git filter-branch -f --tree-filter "rm -f 'backups/mtf20241014- 73045.sql'" HEAD
+git filter-branch -f --tree-filter "rm -f 'backups/mtf20241207- 73045.sql'" HEAD
+git filter-branch -f --tree-filter "rm -f 'backups/mtf20241212 - 51117.sql'" HEAD
+pause
